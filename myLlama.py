dbc415e30e3b98ba16f78b626a52c962fbd3fbac
nurciuoli <naurciuoli@gmail.com>
2024-04-27 12:54:26 -0400

    add image handling

--- myLlama.py
+++ myLlama.py
@@ -1,5 +1,5 @@
 import ollama as o
-
+import base64
 #chat func
 def chat(user_msg,stream=True):
     messages = [
@@ -20,3 +20,14 @@ def generate(prompt,stream=True):
     for part in o.generate('llama3', prompt, stream=stream):
         print(part['response'], end='', flush=True)
 
+
+def generate_w_images(prompt,images,stream=True):
+    for response in o.generate('llava', prompt, images=images, stream=stream):
+        print(response['response'], end='', flush=True)
+    print()
+
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
